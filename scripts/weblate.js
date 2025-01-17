f94e9b3fcdf848e0d7e6f13ce5ef4cd6f7f26f47
Cl3tus <osternaud.clement@gmail.com>
2025-01-17 17:01:07 +0100

    fix: remove plain key

--- scripts/weblate.js
+++ scripts/weblate.js
@@ -1,8 +1,12 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
+/* eslint-disable @typescript-eslint/no-require-imports */
+
 const weblateSyncLocal = require('weblate-sync-tool');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const config = {
-  weblateApiKey: 'wlu_PbKnP2tzLvW79vbGFEZemelrpIWnWIXJlCtJ',
+  weblateApiKey: process.env.WEBLATE_API_KEY_WEB,
   weblateProjectUrl: 'https://weblate.cl3tusdev.com/api',
   weblateProject: 'blindtus',
   weblateComponent: 'web',
